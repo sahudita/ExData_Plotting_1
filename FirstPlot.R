b6e68a8e3809deb01421673db0181b055e7afdda
Suyash Gour A4-66 <139151852+Suyash-Gour-B-56@users.noreply.github.com>
2025-03-13 00:00:49 +0530

    Create FirstPlot.R

---
+++ FirstPlot.R
@@ -0,0 +1,9 @@
+#Suyash Gour 12/3/25
+# Set the Working Directory
+setwd("~/GitHub/ExData_Plotting1")
+
+source("LoadData.R")
+png(filename = "Plot1.png", width = 480, height = 480, units = "px")
+hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", breaks = 12, ylim = c(0, 1200))
+dev.off()
+rm(data)
