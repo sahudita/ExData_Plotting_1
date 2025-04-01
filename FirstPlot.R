29f27020682412ea61cd8de7ff9df0db9a5941b0
sahudita <sahup_1@rknec.edu>
2025-04-01 16:07:49 +0530

    Update FirstPlot.R

--- FirstPlot.R
+++ FirstPlot.R
@@ -3,7 +3,7 @@
 setwd("~/GitHub/ExData_Plotting1")
 
 source("LoadData.R")
-png(filename = "Plot1.png", width = 480, height = 480, units = "px")
-hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", breaks = 12, ylim = c(0, 1200))
+png(filename = "Plot1.png", width = 480 , height = 480 , units = "px")
+hist(Global_active_power , col = "red" , main = "Global Active Power" , xlab = "Global Active Power (kilowatts)" , breaks = 12 , ylim = c(0, 1200))
 dev.off()
 rm(data)
