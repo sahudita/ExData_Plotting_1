ed4471e6f9d8a4de68014942b114c1cc070dcbb1
sahudita <sahup_1@rknec.edu>
2025-04-01 16:10:02 +0530

    Update LoadingData.R

--- LoadingData.R
+++ LoadingData.R
@@ -4,7 +4,7 @@ setwd("~/GitHub/ExData_Plotting1")
 
 if (!file.exists("./data/PowerConsumption.csv")) {
   # Read the Data to a data frame
-  data <- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";", na = "?", colClasses = c(rep("character", 2), rep("numeric", 7)))
+  data <- read.table("./data/household_power_consumption.txt", header = TRUE , sep = ";" , na = "?" , colClasses = c(rep("character", 2) , rep("numeric", 7)))
   attach(data)
   data <- data[(Date == "1/2/2007" | Date == "2/2/2007"), ]
   attach(data)
